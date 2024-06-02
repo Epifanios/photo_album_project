f10274c1d07852ef33da33550d7d99503b741f9c
Epifanios <epifaniosxe@gmail.com>
2024-06-02 16:38:50 +0300

    Add swagger and use cors

--- PhotoAlbums/Program.cs
+++ PhotoAlbums/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using PhotoAlbums.Data;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
@@ -11,11 +12,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Photo Albums API", Version = "v1" });
+});
+
 builder.Services.AddSpaStaticFiles(configuration =>
 {
     configuration.RootPath = "web/build";
 });
 
+// Connect to Database
+// Register the DbContext with the connection string from appsettings.json 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
@@ -23,6 +34,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 else
 {
@@ -35,11 +55,14 @@ app.UseStaticFiles();
 app.UseSpaStaticFiles();
 
 app.UseRouting();
+app.UseCors("CorsPolicy");
 
 app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller}/{action=Index}/{id?}");
+    });
 
 app.UseSpa(spa =>
 {
