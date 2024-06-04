edb06735611042e72adab8cbec3399a91a73c2ac
Epifanios <epifaniosxe@gmail.com>
2024-06-04 19:39:25 +0300

    Create Migrations for Photos

--- PhotoAlbums/Data/ApplicationDbContext.cs
+++ PhotoAlbums/Data/ApplicationDbContext.cs
@@ -10,5 +10,17 @@ namespace PhotoAlbums.Data
         }
 
         public DbSet<Album> Albums { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>()
+                .HasMany(a => a.PhotoAlbums)
+                .WithOne(b => b.Album)
+                .HasForeignKey(b => b.AlbumId)
+                .OnDelete(DeleteBehavior.Cascade);  // Optional: Configure delete behavior
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- PhotoAlbums/Migrations/20240603155702_AddPhotosTable.Designer.cs
+++ PhotoAlbums/Migrations/20240603155702_AddPhotosTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhotoAlbums.Data;
 
@@ -10,9 +11,11 @@ using PhotoAlbums.Data;
 namespace PhotoAlbums.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240603155702_AddPhotosTable")]
+    partial class AddPhotosTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
