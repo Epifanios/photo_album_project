93f830e0d4aec6767556cf8e1d74e03d26f10ac8
Epifanios <epifaniosxe@gmail.com>
2024-06-06 04:18:46 +0300

    Update npm & add css

--- PhotoAlbums/web/src/App.js
+++ PhotoAlbums/web/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
 import Users from './pages/Users';
+import Albums from './pages/Albums';
+import PageNotFound from './pages/PageNotFound';
+import Photos from './pages/Photos';
+import { fetchUsers, fetchAlbums } from './Api';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {    
+        const data = await fetchUsers();
+        setUsers(data);
+    };
+
+    loadUsers();
+  }, []);
+  console.log(users);
+
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-        </ul>
-      </nav>
+    <div className="App">    
       <Routes>
-        <Route path="/" element={<Users />} />
+        <Route exact path="/" element={<Users />} />
+        <Route path="user/:userId/albums" element={<Albums/>} />
+        <Route path="user/:userId/album/:albumId/photos" element={<Photos/>} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
--- PhotoAlbums/web/src/index.js
+++ PhotoAlbums/web/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../src/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
